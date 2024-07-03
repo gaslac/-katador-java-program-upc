c365899caf372b3533cc91f21b6750c07cf7f728
gaslac <91355472+gaslac@users.noreply.github.com>
2024-07-02 22:53:53 -0500

    Actualización de hora en tiempo real
    Desarrollar una aplicación que simplifique la administración del soporte técnico para la instalación de impresoras y otros dispositivos tecnológicos.

---
+++ Main.java
@@ -0,0 +1,8 @@
+package PROYECUPC;
+public class Main {
+    public static void main(String[] args) {
+        MenuPrincipal menuPrincipal = new MenuPrincipal();
+        menuPrincipal.mostrarMenu();
+
+    }
+}
